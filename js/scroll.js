ba68d2d51d52e86ab5bed237d745028e477cee35
Bärtschi Lars <bbaerl@bbcnet.ch>
2017-06-23 08:41:06 +0200

    + Fixed wrong calculation

--- js/scroll.js
+++ js/scroll.js
@@ -2,7 +2,7 @@ var offset = $(".page-title").offset();
 $(window).scroll(function(event) {
   var st = $(this).scrollTop();
   var max = document.documentElement.clientHeight*0.65;
-  var maxMargin = document.documentElement.clientWidth-$(".page-title").width()*1.2;
+  var maxMargin = $(".content").width()-$(".page-title").width()*1.1;
   $(".page-title").css("margin-right", st/max*maxMargin+$(".page-title").width()*0.1);
   console.log(max, st, maxMargin);
 });
