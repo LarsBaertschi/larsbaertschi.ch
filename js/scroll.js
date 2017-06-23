a049ef46bfe561399d0dc7abe331801dfcbe2fb4
Bärtschi Lars <bbaerl@bbcnet.ch>
2017-06-23 08:12:01 +0200

    + Added content

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,10 @@
 var offset = $(".page-title").offset();
 $(window).scroll(function(event) {
   var st = $(this).scrollTop();
-  $(".page-title").css("margin-right", st * 1.5 + 30);
-  console.log(st);
+  var max = document.documentElement.clientHeight*0.65;
+  var maxMargin = document.documentElement.clientWidth-$(".page-title").width()*1.2;
+  $(".page-title").css("margin-right", st/max*maxMargin+$(".page-title").width()*0.1);
+  console.log(max, st, maxMargin);
 });
+
+$(window).scroll();
